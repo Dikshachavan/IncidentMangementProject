be29a631f6144aa1a18c3a96d55f9cc9b87f9ad1
Anjan O <anjan.o@slkgroup.com>
2020-06-02 16:03:09 +0530

    working trackincidents

--- Controllers/trackController.cs
+++ Controllers/trackController.cs
@@ -1,30 +1,32 @@
-﻿using System;
+﻿using NewDemoProject.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
 using NewDemoProject.Models;
+
 
 namespace NewDemoProject.Controllers
 {
     public class trackController : Controller
     {
+        public static string mainConn = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+        SqlConnection connection = new SqlConnection(mainConn);
+        status status = new status();
         // GET: treack
         public ActionResult trackstatus(int trackno)
         {
-            status status = new status();
-            string db = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
-            using (SqlConnection con = new SqlConnection(db))
-            {
+            try {
+                SqlCommand command = new SqlCommand("track_incident", connection);
                 DataSet ds = new DataSet();
-                SqlCommand cmd = new SqlCommand("track_incident", con);
-                cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@incident_id", trackno);
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                con.Open();
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@incident_id", trackno);
+                SqlDataAdapter da = new SqlDataAdapter(command);
+                connection.Open();
                 da.Fill(ds);
                 List<status> statuslist = new List<status>();
 
@@ -37,41 +39,63 @@ namespace NewDemoProject.Controllers
                 }
 
                 status.statuses = statuslist;
-                con.Close();
 
+                return View(status);
             }
-            return View(status);
-
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                connection.Close();
+            }
 
         }
+        
+
+
 
 
 
         public ActionResult listtickets(string employeemailid)
         {
+        try
+        {
             incidentlist incidents = new incidentlist();
-            string db = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
-            using (SqlConnection con = new SqlConnection(db))
-            {
-                DataSet ds = new DataSet();
-                SqlCommand cmd = new SqlCommand("listincidents", con);
-                cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@employee_mail_id", employeemailid);
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                con.Open(); da.Fill(ds);
-                List<incidentlist> incidentlists = new List<incidentlist>();
+            DataSet ds = new DataSet();
+            SqlCommand cmd = new SqlCommand("listincidents", connection);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@employee_mail_id", employeemailid);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            connection.Open(); da.Fill(ds);
+            List<incidentlist> incidentlists = new List<incidentlist>();
 
-                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
-                {
-                    incidentlist incidentlist = new incidentlist();
-                    incidentlist.incidentID = Convert.ToInt32(ds.Tables[0].Rows[i]["incident_id"]);
-                    incidentlist.title = ds.Tables[0].Rows[i]["incident_title"].ToString();
-                    incidentlists.Add(incidentlist);
-                }
-                incidents.incidentarray = incidentlists;
-                return View(incidents);
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+                incidentlist incidentlist = new incidentlist();
+                incidentlist.incidentID = Convert.ToInt32(ds.Tables[0].Rows[i]["incident_id"]);
+                incidentlist.title = ds.Tables[0].Rows[i]["incident_title"].ToString();
+                incidentlists.Add(incidentlist);
+            }
+            incidents.incidentarray = incidentlists;
+            return View(incidents);
+        }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+            catch (Exception e)
+        {
+            throw e;
+        }
+            
+        finally
+        {
+                connection.Close();
+         }
             }
 
         }
     }
-}
+
